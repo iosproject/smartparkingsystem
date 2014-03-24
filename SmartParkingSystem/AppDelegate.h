98cbe389aadf900ac1f768f8693fe862c97accfe
Computer Science1 <jlisojo@kean.edu>
2014-03-24 12:12:23 -0400

    Initial Commit

---
+++ SmartParkingSystem/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmartParkingSystem
+//
+//  Created by Computer Science on 3/24/14.
+//  Copyright (c) 2014 Computer Science. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmartParkingSystem/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SmartParkingSystem
+//
+//  Created by Computer Science on 3/24/14.
+//  Copyright (c) 2014 Computer Science. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
