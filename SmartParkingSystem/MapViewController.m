8d99f159a6e5debb01bdf4f0a51016640833e141
Josh Lisojo <jlisojo@gmail.com>
2014-04-16 12:57:10 -0400

    sub 10 for unavailable spots

--- SmartParkingSystem/MapViewController.m
+++ SmartParkingSystem/MapViewController.m
@@ -259,7 +259,7 @@
             int y = [[spot objectForKey:@"position_y"] intValue];
             
             // Draw a circle (filled) (x, y, w, h)
-            CGContextFillEllipseInRect(ctx, CGRectMake(x, y, 20.0, 20.0));
+            CGContextFillEllipseInRect(ctx, CGRectMake(x-10, y, 20.0, 20.0));
         }
     }
     
